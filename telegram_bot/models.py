fbf28168f8d684cdfcdd612aa3e4a6f008f24213
damodara <jay108@mail.ru>
2026-03-24 11:24:56 +0300

    внес обновления

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
+    is_registered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} (@{self.username})"
